1e8024deb5da8efa734c4548b6642125fd0edf32
Henry Skiba <henry.skiba@frgrisk.com>
2025-10-29 11:31:09 +0800

    add polymorphic sample

--- factor/models.py
+++ factor/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from polymorphic.models import PolymorphicModel
 
-# Create your models here.
+class Base(PolymorphicModel):
+    name = models.CharField(max_length=32)
+
+class FX(Base):
+    to_currency = models.CharField(max_length=3)
+    from_currency = models.CharField(max_length=3)
+
+class Volatility(Base):
+    base_factor = models.ForeignKey(Base, on_delete=models.CASCADE)
